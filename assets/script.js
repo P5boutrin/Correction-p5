9361b1d3a82ae92f0546ff58277246460d097ff1
André Boutrin <andre.boutrin@gmail.com>
2023-06-16 09:14:47 +0200

    Vendredi

--- assets/script.js
+++ assets/script.js
@@ -21,10 +21,10 @@ const slides = [
 // Variable
 const bannerImg = document.querySelector(".banner-img");
 const txtImg = document.querySelector("p");
-const dotImgs = document.querySelectorAll(".dot");
+const dotImg = document.querySelectorAll(".dot");
 
 // structure: 1:element html  2:ecoute  3:type click   4:function a executer
-// ARROW:right
+
 document.querySelector(".arrow_right").addEventListener("click", next);
 let index = 0;
 function next() {
@@ -35,11 +35,9 @@ function next() {
   }
   bannerImg.src = "./assets/images/slideshow/" + slides[index].image;
   txtImg.innerHTML = slides[index].tagLine;
-  // // dotImgs. = + slides[e.target.dataset.order]
-  // clickedDot.classList.add = + slides[index].e.target.dataset.order
 }
 
-// ARROW:right
+
 document.querySelector(".arrow_left").addEventListener("click", previous);
 function previous() {
   if (index < slides.length - 1) {
@@ -51,24 +49,8 @@ function previous() {
   txtImg.innerHTML = slides[index].tagLine;
 }
 
-// //  
-// function dotSelected(e) {
-//   const clickedDot = e.target
-//   const previousClickedDot = document.querySelector(".dot_selected")
-//   if(previousClickedDot){
-//     previousClickedDot.classList.remove("dot_selected")
-//   }
-
-//   clickedDot.classList.add("dot_selected")
-
-//   const orderIndex = parseInt(clickedDot.dataset.order, 10);
-//   bannerImg.src = "./assets/images/slideshow/" + slides[orderIndex].image;
-//   txtImg.innerHTML
-//   }
-
-// DOTS   andre
-
-dotImgs.forEach((dotImg, i) => {
+// DOTS
+dotImg.forEach((dotImg, i) => {
   dotImg.dataset.order = i;
   dotImg.addEventListener("click", dotSelected);
 });
@@ -79,9 +61,6 @@ function dotSelected(e) {
     previousClickedDot.classList.remove("dot_selected");
   }
   clickedDot.classList.add("dot_selected");
-
-  document.getElementsByClassName("dot").className = "dote_selected";
-  
 
   bannerImg.src = "./assets/images/slideshow/" + slides[e.target.dataset.order].image;
   txtImg.innerHTML = slides[e.target.dataset.order].tagLine;
